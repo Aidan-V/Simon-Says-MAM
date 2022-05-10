83eec5e3c23c6a45ee0befe40aee1dc66997fd1f
Aidan-V <89591050+Aidan-V@users.noreply.github.com>
2022-05-10 07:53:57 -0500

    Update ButtonSetting.cs

--- ButtonSetting.cs
+++ ButtonSetting.cs
@@ -1,13 +1,13 @@
-using UnityEngine;
-using System;
-using System.Collections;
+using UnityEngine
+using System
+using System.Collections
 
 [Serializable]
 // Serializable means to basically store info or bytes to the file for use
 public class ButtonSetting
 {
 
-    public Color normalColor;
-    public Color highlightColor;
+    public Color normalColor
+    public Color highlightColor
 
 }
